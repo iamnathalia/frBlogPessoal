2d37710a02563203b2acd409b80fbf74c9bae2da
Nathalia <nat_sou@hotmail.com>
2022-05-10 11:30:18 -0300

    implementação de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component'; /* (2) após a inclusão da rota, o componente é importado */
 
-const routes: Routes = [];
+// Rota dos componentes em Angular é um objeto!
+/*array (listagem) de rotas para inclusão dos componentes*/
+const routes: Routes = [ //toda vez que um novo componente for criado, deve incluir na rota:
+
+    {path:'', redirectTo: 'login', pathMatch: 'full'}, /* toda vez que o endereço for "vazio" ex. localhost:4200 (end sem "/") será direc. a tela login */
+    {path:'login', component: LoginComponent}, /* (1) atributo dos objetos: path é a prop. que a rota a deve ser inclusa + component: nome atribuido no app-nomecomponente */
+    {path:'cadastrar', component: CadastrarComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
